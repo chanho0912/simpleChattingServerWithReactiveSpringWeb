6eb093ee8a6591106bead036ba8f3317b740d64b
chanho0912 <chanho0912@naver.com>
2021-11-05 17:04:31 +0900

    Initial commit

---
+++ src/main/java/com/example/mungboon/MungboonApplication.java
@@ -0,0 +1,12 @@
+package com.example.mungboon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungboonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungboonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/mungboon/MungboonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mungboon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungboonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
